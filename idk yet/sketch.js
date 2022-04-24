132bb10f11a73d90724b6f7090bbdc628396fc5e
166236uwm <166236@student.uwm.edu.pl>
2022-04-25 00:37:30 +0200

    wandering trader

--- idk yet/sketch.js
+++ idk yet/sketch.js
@@ -1,7 +1,102 @@
+let points = []
+let order = []
+let bestOrder = []
+let len
+let index = 0
+
 function setup() {
-  // put setup code here
+  colorMode("HSB")
+  createCanvas(1024, 512)
+  background(0)
+  points.push(createVector(random(0, width), random(0, height)))
+  order.push(points.length - 1)
+  bestOrder = order.slice()
 }
 
 function draw() {
-  // put drawing code here
+  background(0)
+  strokeWeight(6)
+  stroke(255)
+  for(let i = 0; i < points.length; i++){
+    point(points[i].x, points[i].y)
+  }
+  noFill()
+  strokeWeight(1)
+  beginShape()
+  for(let i = 0; i < order.length; i++){
+    vertex(points[order[i]].x, points[order[i]].y)
+  }
+  endShape()
+  strokeWeight(3)
+  beginShape()
+  for(let i = 0; i < order.length; i++){
+    vertex(points[bestOrder[i]].x, points[bestOrder[i]].y)
+  }
+  endShape()
+  shuffleOrder()
+  index++
+  if(calcDist() < len){
+    len = calcDist()
+    bestOrder = order.slice()
+  }
+  let permut = factorial(points.length - 1)
+  let str = (index / permut) * 100
+  str = String(nf(str, 0, 2)) + '%'
+  textSize(40)
+  text(str, 40, 40)
+}
+
+function mousePressed(){
+  points.push(createVector(mouseX, mouseY))
+  order.push(points.length - 1)
+  len = calcDist()
+  bestOrder = order.slice()
+  index = 0
+}
+
+function shuffleOrder(){
+  let x = -1;
+  //https://www.quora.com/How-would-you-explain-an-algorithm-that-generates-permutations-using-lexicographic-ordering
+  for(let i = 0; i < order.length - 1; i++){
+    if(order[i] < order[i + 1]){
+      if(i > x){
+        x = i
+      }
+    }
+  }
+  if(x == -1 && points.length > 4){
+    noLoop()
+    print("done")
+  }
+  let y = -1
+  for(let i = 0; i < order.length; i++){
+    if(order[x] < order[i] && i > y){
+      y = i
+    }
+  }
+  swap(order, x, y)
+  let endOfArray = order.splice(x + 1)
+  endOfArray.reverse()
+  order = order.concat(endOfArray)
+}
+
+function swap(arr, x, y){
+  let n = arr[x]
+  arr[x] = arr[y]
+  arr[y] = n
+}
+
+function calcDist(){
+  let sum = 0
+  for(let i = 0; i < points.length - 1; i++){
+    sum += dist(points[order[i]].x, points[order[i]].y, points[order[i + 1]].x, points[order[i + 1]].y)
+  }
+  return sum
+}
+
+function factorial(n){
+  if(n == 1 || n == 0){
+    return 1
+  }
+  return n * factorial(n - 1)
 }
